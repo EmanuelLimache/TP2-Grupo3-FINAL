2235262c788d9a5b7ce4cd7f49499f98ab58ce3c
Enamuel L <Enamuel L@EmAnUeL>
2022-05-06 01:57:08 -0300

    ejercicio 10

---
+++ TP2_Grupo3-FINAL/src/main/java/fi/unju/edu/ar/punto10/Principal10.java
@@ -0,0 +1,14 @@
+package fi.unju.edu.ar.punto10;
+
+public class Principal10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=320;
+		while(i>=160 && i<=320) {
+			System.out.println(""+i);
+			i=i-20;
+		}
+	}
+
+}
