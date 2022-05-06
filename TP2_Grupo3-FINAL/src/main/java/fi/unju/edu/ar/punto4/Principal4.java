6fe6e56c89580845c1764b3511fd8e6b0357576a
Enamuel L <Enamuel L@EmAnUeL>
2022-05-06 01:54:18 -0300

    ejercicio 4

---
+++ TP2_Grupo3-FINAL/src/main/java/fi/unju/edu/ar/punto4/Principal4.java
@@ -0,0 +1,11 @@
+package fi.unju.edu.ar.punto4;
+
+public class Principal4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numero num1= new Numero();
+		num1.NumeroCorrespoondeAunMes();
+	}
+
+}
