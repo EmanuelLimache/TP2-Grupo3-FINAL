7a671f8ebfc8c5047202949834969cfb24048278
Enamuel L <Enamuel L@EmAnUeL>
2022-05-06 01:55:31 -0300

    ejercicio 6

---
+++ TP2_Grupo3-FINAL/src/main/java/fi/unju/edu/ar/punto6/Principal6.java
@@ -0,0 +1,11 @@
+package fi.unju.edu.ar.punto6;
+
+public class Principal6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Alumno nuevo=new Alumno();
+		nuevo.estadoDeUnAlumno();
+	}
+
+}
